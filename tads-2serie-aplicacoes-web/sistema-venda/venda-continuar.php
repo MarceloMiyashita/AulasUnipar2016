6979b6c4474a635bb202007d9f3486aac84cbdeb
Alisson Chiquitto <chiquitto@gmail.com>
2016-11-09 17:48:00 -0200

    Sistema venda

--- tads-2serie-aplicacoes-web/sistema-venda/venda-continuar.php
+++ tads-2serie-aplicacoes-web/sistema-venda/venda-continuar.php
@@ -5,11 +5,28 @@ require './config.php';
 require './lib/funcoes.php';
 require './lib/conexao.php';
 
+// Verificar se a venda existe
+$idvenda = (int) $_GET['idvenda'];
+$sql = "Select status From venda
+Where idvenda = $idvenda";
+$resultado = mysqli_query($con, $sql);
+$venda = mysqli_fetch_assoc($resultado);
+
+if (!$venda) {
+  javascriptAlertFim('Venda inexistente', 'vendas.php');
+}
+
+// Verificar se a venda esta aberta
+if ($venda['status'] == VENDA_FECHADA) {
+  javascriptAlertFim('Venda esta fechada', 'vendas.php');
+}
+
+// Atribuir na sessao o idvenda
+$_SESSION['idvenda'] = $idvenda;
+header('location:venda-produto.php');
 
 
 
 
 
-
-
-
+//
--- tads-2serie-aplicacoes-web/sistema-venda/venda-fechar.php
+++ tads-2serie-aplicacoes-web/sistema-venda/venda-fechar.php
@@ -5,4 +5,18 @@ require './config.php';
 require './lib/funcoes.php';
 require './lib/conexao.php';
 
+$idvenda = $_SESSION['idvenda'];
+$status = VENDA_FECHADA;
 
+$sql = "Update venda Set status='$status'
+Where idvenda = $idvenda";
+mysqli_query($con, $sql);
+
+unset($_SESSION['idvenda']);
+
+header('location:vendas.php');
+
+
+
+
+//
